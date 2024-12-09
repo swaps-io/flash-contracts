b8985e08d23f0812e543711ca7f72ad90cf08570
Alexey Bogdanov <alexey.bogdanov.97@gmail.com>
2024-12-09 13:30:44 +0400

    Update manual native etimator facet

--- contracts/order/OrderReceiveManualNativeEstimatorFacet.sol
+++ contracts/order/OrderReceiveManualNativeEstimatorFacet.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.8.24;
 
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
 import {BitStorageLib} from "../storage/BitStorageLib.sol";
 
 import {EnvLib} from "../utils/EnvLib.sol";
@@ -15,22 +17,36 @@ import {IOrderReceiveManualNativeEstimator} from "./interfaces/IOrderReceiveManu
 
 import {OrderHashLib, Order} from "./OrderHashLib.sol";
 import {OrderReceiverLib} from "./OrderReceiverLib.sol";
+import {ManualNativeNonceLib} from "./ManualNativeNonceLib.sol";
+import {OrderReceiverManualNativeLib} from "./OrderReceiverManualNativeLib.sol";
 
 contract OrderReceiveManualNativeEstimatorFacet is IOrderReceiveManualNativeEstimator, Estimator {
-    function estimateReceiveOrderAssetManualNative(Order calldata order_, bytes calldata toSignature_, address caller_) external payable onlyEstimate {
-        _estimateReceiveOrderAssetManualNative(order_, toSignature_, caller_, NativeLib.VALUE_ORIGINAL_BIT);
+    function estimateReceiveOrderAssetManualNative(
+        Order calldata order_,
+        bytes calldata toSignature_,
+        bytes calldata toPostData_,
+        address caller_
+    ) external payable onlyEstimate {
+        _estimateReceiveOrderAssetManualNative(order_, toSignature_, toPostData_, caller_, NativeLib.VALUE_ORIGINAL_BIT);
     }
 
     function estimateReceiveOrderAssetManualNative(
         Order calldata order_,
         bytes calldata toSignature_,
+        bytes calldata toPostData_,
         address caller_,
         uint256 value_
     ) external payable onlyEstimate {
-        _estimateReceiveOrderAssetManualNative(order_, toSignature_, caller_, value_);
+        _estimateReceiveOrderAssetManualNative(order_, toSignature_, toPostData_, caller_, value_);
     }
 
-    function _estimateReceiveOrderAssetManualNative(Order calldata order_, bytes calldata toSignature_, address caller_, uint256 value_) private {
+    function _estimateReceiveOrderAssetManualNative(
+        Order calldata order_,
+        bytes calldata toSignature_,
+        bytes calldata toPostData_,
+        address caller_,
+        uint256 value_
+    ) private {
         if (!EnvLib.isActiveDeadline(order_.deadline)) revert OrderReceiveExpired();
         if (caller_ != order_.fromActor) revert ReceiveCallerMismatch();
         if (!EnvLib.isThisChain(order_.fromChain)) revert ReceiveChainMismatch();
@@ -41,11 +57,26 @@ contract OrderReceiveManualNativeEstimatorFacet is IOrderReceiveManualNativeEsti
         if (BitStorageLib.hasBitStored(orderReceiveEventHash)) revert OrderAlreadyReceived();
 
         if (toSignature_.length != 0) SignatureLib.validateSignature(orderHash, toSignature_, order_.toActor);
+        ManualNativeNonceLib.validatePostData(order_.nonce, toPostData_);
 
         OrderReceiverLib.store().collateralLocker.commitLock(order_.toActor, order_.collateralAmount, order_.collateralChain, order_.collateralUnlocked);
         BitStorageLib.storeBit(orderReceiveEventHash);
 
-        NativeLib.transferFrom(caller_, order_.toActor, order_.fromAmount, value_);
+        if (toPostData_.length == 0) NativeLib.transferFrom(caller_, order_.toActor, order_.fromAmount, value_);
+        else {
+            OrderReceiverManualNativeLib.store().activeOrderHash[order_.toActor] = orderHash;
+
+            if (order_.nonce & ManualNativeNonceLib.POST_WITH_SEND_BIT != 0) {
+                NativeLib.transferFrom(msg.sender, order_.toActor, order_.fromAmount, value_, toPostData_);
+            } else {
+                NativeLib.transferFrom(msg.sender, order_.toActor, order_.fromAmount, value_);
+
+                (bool postSuccess, bytes memory postResult) = order_.toActor.call(toPostData_);
+                if (order_.nonce & ManualNativeNonceLib.POST_ALLOW_FAIL_BIT == 0) Address.verifyCallResultFromTarget(order_.toActor, postSuccess, postResult);
+            }
+
+            delete OrderReceiverManualNativeLib.store().activeOrderHash[order_.toActor];
+        }
 
         emit AssetReceive(orderHash);
     }
--- contracts/order/interfaces/IOrderReceiveManualNativeEstimator.sol
+++ contracts/order/interfaces/IOrderReceiveManualNativeEstimator.sol
@@ -7,7 +7,18 @@ import {IEstimator} from "../../utils/interfaces/IEstimator.sol";
 import {IOrderReceiverManualNativeErrors, IOrderReceiverErrors, IOrderReceiverEvents, Order} from "./IOrderReceiverManualNative.sol";
 
 interface IOrderReceiveManualNativeEstimator is IEstimator, IOrderReceiverManualNativeErrors, IOrderReceiverErrors, IOrderReceiverEvents {
-    function estimateReceiveOrderAssetManualNative(Order calldata order, bytes calldata toSignature, address caller) external payable;
+    function estimateReceiveOrderAssetManualNative(
+        Order calldata order,
+        bytes calldata toSignature,
+        bytes calldata toPostData,
+        address caller
+    ) external payable;
 
-    function estimateReceiveOrderAssetManualNative(Order calldata order, bytes calldata toSignature, address caller, uint256 value) external payable;
+    function estimateReceiveOrderAssetManualNative(
+        Order calldata order,
+        bytes calldata toSignature,
+        bytes calldata toPostData,
+        address caller,
+        uint256 value
+    ) external payable;
 }
